be52c1ea4dda5a88de33593075145f4bfd2e93a8
Dror <dror.rosentraub@gmail.com>
2024-05-06 17:54:23 +0700

    Update streamlit_weather_app.py

--- streamlit_weather_app.py
+++ streamlit_weather_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 from Functions import *
-@st.cache_data()
+
 def train_and_fit_models():
+    # Function to train and fit models
     full_data = train_models()
     stacking_predictor = StackingPollutantPredictor(full_data)
     stacking_predictor.fit()
@@ -17,11 +18,16 @@ def predict_pollution(model, data):
 st.title("Pollution Prediction App")
 st.sidebar.header("User Input Coordinates")
 
+# Button to train the model, only trains once if the model isn't already in the session state
+if 'model' not in st.session_state or st.sidebar.button('Train Model'):
+    st.session_state.model = train_and_fit_models()
+    st.sidebar.success('Model Trained Successfully!')
+
 with st.sidebar.form(key='client_info_form'):
     latitude = st.number_input("Latitude",  value=0.0, step=0.1)
-    longitude = st.number_input("Longitude",value=0.0, step=0.1)
-    elevation = st.number_input("Elevation",value=0.0, step=0.1)
-    submit_button = st.form_submit_button(label='Run the weather prediction')
+    longitude = st.number_input("Longitude", value=0.0, step=0.1)
+    elevation = st.number_input("Elevation", value=0.0, step=0.1)
+    submit_button = st.form_submit_button(label='Run the pollution prediction')
 
 if submit_button:
     # Displaying input data
@@ -38,11 +44,12 @@ if submit_button:
         'closest_city_km': [calculate_distances_to_cities([[latitude, longitude]])]
     })
 
-    # Getting the cached model
-    model = train_and_fit_models()
+    # Ensure model is trained
+    if 'model' in st.session_state:
+        # Predicting pollution
+        prediction = predict_pollution(st.session_state.model, data_to_predict)
 
-    # Predicting pollution
-    prediction = predict_pollution(model, data_to_predict)
-
-    # Display the prediction
-    st.write("Predicted Pollution Levels:", prediction)
+        # Display the prediction
+        st.write("Predicted Pollution Levels:", prediction)
+    else:
+        st.error("Model not trained. Please train the model first.")
